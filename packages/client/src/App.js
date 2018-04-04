bc3f3f26e60dc7631eed92132aaccae516fda65a
JulianMayorga <mayorga.julian@gmail.com>
2018-04-04 18:24:33 -0300

    Subscribe to new pins
    This allows us to remove refetchQueries

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -13,6 +13,14 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:3001/subscriptions`,
+  options: {
+    reconnect: true
+  }
+});
 
 const client = new ApolloClient({
   link: ApolloLink.from([
@@ -25,6 +33,7 @@ const client = new ApolloClient({
         );
       if (networkError) console.log(`[Network error]: ${networkError}`);
     }),
+    wsLink,
     new HttpLink({
       uri: "http://localhost:3001/graphql",
       credentials: "same-origin"
@@ -44,9 +53,20 @@ const PINS_QUERY = gql`
   }
 `;
 
+const PINS_SUBSCRIPTION = gql`
+  subscription {
+    pinAdded {
+      title
+      link
+      image
+      id
+    }
+  }
+`;
+
 const PinsQuery = ({ children }) => (
   <Query query={PINS_QUERY}>
-    {({ loading, error, data }) => {
+    {({ loading, error, data, subscribeToMore }) => {
       if (loading)
         return (
           <div style={{ paddingTop: 20 }}>
@@ -54,8 +74,22 @@ const PinsQuery = ({ children }) => (
           </div>
         );
       if (error) return <p>Error :(</p>;
+      const subscribeToMorePins = () => {
+        subscribeToMore({
+          document: PINS_SUBSCRIPTION,
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data || !subscriptionData.data.pinAdded)
+              return prev;
+            const newPinAdded = subscriptionData.data.pinAdded;
 
-      return children(data.pins);
+            return Object.assign({}, prev, {
+              pins: [...prev.pins, newPinAdded]
+            });
+          }
+        });
+      };
+
+      return children(data.pins, subscribeToMorePins);
     }}
   </Query>
 );
@@ -67,7 +101,6 @@ const AddPinMutation = ({ children }) => (
         addPin(title: $title, link: $link, image: $image)
       }
     `}
-    refetchQueries={[{ query: PINS_QUERY }]}
   >
     {(addPin, { data, loading, error }) =>
       children(addPin, { data, loading, error })
@@ -76,25 +109,35 @@ const AddPinMutation = ({ children }) => (
 );
 
 class App extends Component {
+  componentDidMount() {
+    this.props.subscribeToMorePins();
+  }
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Container>
-          <PinsQuery>{pins => <PinListPage pins={pins} />}</PinsQuery>
-          <AddPinMutation>
-            {(addPin, { data, loading, error }) => (
-              <AddPinPage
-                addPin={({ title, link, image }) =>
-                  addPin({ variables: { title, link, image } })
-                }
-              />
-            )}
-          </AddPinMutation>
-          <Nav />
-        </Container>
-      </ApolloProvider>
+      <Container>
+        <PinListPage pins={this.props.pins} />
+        <AddPinMutation>
+          {(addPin, { data, loading, error }) => (
+            <AddPinPage
+              addPin={({ title, link, image }) =>
+                addPin({ variables: { title, link, image } })
+              }
+            />
+          )}
+        </AddPinMutation>
+        <Nav />
+      </Container>
     );
   }
 }
 
-export default App;
+export default () => (
+  <ApolloProvider client={client}>
+    <PinsQuery>
+      {/* Wrap App with PinsQuery because we need to access subscribeToMorePins in componentDidMount */}
+      {(pins, subscribeToMorePins) => (
+        <App pins={pins} subscribeToMorePins={subscribeToMorePins} />
+      )}
+    </PinsQuery>
+  </ApolloProvider>
+);
