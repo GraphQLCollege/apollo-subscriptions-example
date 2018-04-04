a907f893e8515bc2cb7ee6926ecee924b3f48d06
JulianMayorga <mayorga.julian@gmail.com>
2018-04-04 14:41:35 -0300

    Export Spinner in components

--- packages/apollo-subscription-example-components/index.js
+++ packages/apollo-subscription-example-components/index.js
@@ -2,3 +2,4 @@ export { default as Container } from "./Container";
 export { default as Nav } from "./Nav";
 export { default as PinListPage } from "./PinListPage";
 export { default as AddPinPage } from "./AddPinPage";
+export { Spinner } from "gestalt";
